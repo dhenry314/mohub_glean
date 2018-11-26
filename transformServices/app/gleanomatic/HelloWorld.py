0098f274caf4058c3c15b4b3f7607d359d26d625
David Henry <dhenry314@gmail.com>
2018-11-26 16:21:46 -0600

    removed _pycache_ dirs and added them to .gitignore

--- transformServices/app/gleanomatic/HelloWorld.py
+++
@@ -1,5 +0,0 @@
-
-class HelloWorld:
-
-    def sayHello(self):
-        return "Hello World again!"
--- transformServices/app/gleanomatic/scripts/URLTest.py
+++
@@ -1,6 +0,0 @@
-import gleanomatic.Utils as Utils
-
-content = Utils.getContent("http://localhost:8080/admin")
-
-print(content)
-
